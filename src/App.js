714ca9060459294e802c982ca9e1b116c18bc5a5
Odoh Friday <odohfriday33@yahoo.com>
2023-03-24 12:42:16 +0100

    new update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React,{useState} from "react";
+import {produce} from "immer"
+import { generate } from "shortid";
 function App() {
+const [people,setPeople] = useState([
+  {
+  
+  id:generate(),
+  firstName:'',
+  LastName:'',
+}
+])
+ 
+console.log(people);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{textAlign:"center"}}>
+      <button onClick={()=>{
+        setPeople(currentPeople => [...currentPeople,{
+          id :generate(),
+          firstName:'',
+          lastName:''
+        }])
+      }}> Add new Person</button>
+      {people.map((p, index)=>{
+        
+        return (
+          <div key={p.id}>
+            <input 
+             onChange={e =>{
+              const firstName = e.target.value;
+              setPeople(currentPeople=>produce(currentPeople, v =>{
+                v[index].firstName = firstName
+              })
+              );
+             }}
+            value={p.firstName} placeholder="First name" />
+            <input 
+             onChange={e =>{
+              const lastName = e.target.value;
+              setPeople(currentPeople=>produce(currentPeople, v =>{
+                v[index].LastName = lastName
+              })
+              );
+             }}
+            value={p.LastName} placeholder="Last name" />
+
+            <button onClick={() =>{
+              setPeople(currentPeople => currentPeople.filter(x => x.id !== p.id))
+            }}>
+              x
+            </button>
+          </div>
+        )
+      })}
     </div>
   );
 }
